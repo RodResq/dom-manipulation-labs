f08aa5615e859f6219b62605537d1dde1c3e0362
rodresq <rodrigoresque@yahoo.com.br>
2020-09-07 21:21:55 -0300

    updt

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,29 +1,41 @@
-import { AfterViewChecked, Component, ElementRef, Renderer2, ViewChildren, QueryList } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, Renderer2, ViewChildren, QueryList, ViewChild, ViewContainerRef, TemplateRef, AfterViewInit } from '@angular/core';
 
 
 @Component({
     selector: 'app-parent',
     template: `<p>Parent Works</p>
-    <app-child #child></app-child>
+    <ng-container #viewcontainer></ng-container>
+    <ng-template>
+        <app-child #child></app-child>
+    </ng-template>
     <button (click)="removeChild()">remove child</button>
     `,
     styleUrls: ['./parent.component.css']
 })
-export class ParentComponent implements AfterViewChecked{
+export class ParentComponent implements AfterViewChecked, AfterViewInit{
 
     @ViewChildren('child', {read: ElementRef}) 
     childComp: QueryList<ElementRef>
+
+    @ViewChild('viewcontainer', {read: ViewContainerRef}) viewcontainer;
+
+    @ViewChild(TemplateRef) template: TemplateRef<null>;
     
     constructor(
         private renderer: Renderer2, 
         private host: ElementRef) {}
+    
+    
+    ngAfterViewInit(): void {
+        this.viewcontainer.createEmbeddedView(this.template);
+    }
 
     ngAfterViewChecked(): void {
-        console.log(this.childComp.length);
+        console.log("The child component count " + this.childComp.length);
     }
 
     removeChild() {
-        this.renderer.removeChild(this.host.nativeElement, this.childComp.first.nativeElement);
+        this.viewcontainer.remove();
     }
  
 }
